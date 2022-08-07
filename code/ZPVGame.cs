aa49f0f003e59d21aa20bd62779b94d18952a14c
ItsRifter <d_thomas_smith30@hotmail.com>
2022-08-07 17:36:37 +0100

    Initial commit

---
+++ code/ZPVGame.cs
@@ -0,0 +1,23 @@
+﻿using Sandbox;
+using Sandbox.UI.Construct;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+public partial class ZPVGame : Sandbox.Game
+{
+	public ZPVGame()
+	{
+
+	}
+
+	public override void ClientJoined( Client client )
+	{
+		base.ClientJoined( client );
+
+		var pawn = new ZPVPawn();
+		pawn.Spawn();
+		client.Pawn = pawn;
+	}
+}
---
+++ code/ZPVPawn.cs
@@ -0,0 +1,82 @@
+﻿using Sandbox;
+using System;
+using System.Linq;
+
+namespace Sandbox;
+
+partial class ZPVPawn : Player
+{
+	public enum ZPVTeamEnum
+	{
+		Unspecified,
+		Spectator,
+		Survivor,
+		Infected,
+		Undead
+	}
+
+	public enum ZombieType
+	{
+		Standard,
+		Carrier
+	} 
+
+	public void SpawnToPoint(ZPVTeamEnum teamPoint = ZPVTeamEnum.Unspecified)
+	{
+		SpawnPoint spawnpoints = null;
+
+		switch (teamPoint)
+		{
+			case ZPVTeamEnum.Unspecified:
+				spawnpoints = Entity.All.OfType<SpawnPoint>().OrderBy( x => Guid.NewGuid() ).FirstOrDefault();
+				break;
+			case ZPVTeamEnum.Survivor:
+				//TODO: Make survivor spawnpoints
+				break;
+			case ZPVTeamEnum.Undead:
+				//TODO: Make zombie spawnpoints
+				break;
+		}
+
+		if ( spawnpoints != null )
+		{
+			var tx = spawnpoints.Transform;
+			tx.Position = tx.Position + Vector3.Up * 50.0f;
+			Transform = tx;
+		}
+	}
+
+	public void Infect()
+	{
+
+	}
+
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		//Temporary, will use Zombie panic playermodels
+		SetModel( "models/citizen/citizen.vmdl" );
+
+		CameraMode = new FirstPersonCamera();
+		Animator = new StandardPlayerAnimator();
+
+		//Temporary, should make a game controller
+		Controller = new WalkController();
+
+		EnableDrawing = true;
+		EnableHideInFirstPerson = true;
+		EnableShadowInFirstPerson = true;
+	}
+
+	public override void Simulate( Client cl )
+	{
+		base.Simulate( cl );
+		SimulateActiveChild( cl, ActiveChild );
+	}
+
+	public override void FrameSimulate( Client cl )
+	{
+		base.FrameSimulate( cl );
+	}
+}
